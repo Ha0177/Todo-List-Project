5988bd7ac7dd1d0474e08845392a54383acdbc1b
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-08-12 17:01:10 +0300

    Add task removal functionality in Project class and add Task class methods

--- src/projectManager.js
+++ src/projectManager.js
@@ -7,4 +7,14 @@ class Project {
     addTask(task) {
         this.tasks.push(task);
     }
+
+    removeTask(taskId) {
+        const taskIndex = this.tasks.findIndex(task => task.id === taskId) 
+
+        if (taskIndex !== -1) {
+            this.tasks.splice(taskIndex, 1);
+            return true;
+        }
+        return false;
+    }
 }
--- src/taskManager.js
+++ src/taskManager.js
@@ -1,5 +1,5 @@
 class Task {
-    constructor(title, description, dueDate, priority, project, complete = false, ) {
+    constructor(title, description, dueDate, priority, project, complete = false ) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
@@ -7,5 +7,49 @@ class Task {
         this.project = project;
         this.complete = complete;
         this.id = crypto.randomUUID();
+    }
+
+    setTitle(newTitle) {
+        if(typeof newTitle === "string" && newTitle.trim() !== "") {
+            this.title = newTitle;
+        } else {
+            return false;
+        }
+    }
+
+    setDescription(newDescription) {
+        if(typeof newDescription === "string" && newDescription.trim() !== "") {
+            this.description = newDescription;
+        } else {
+            return false;
+        }
+    }
+
+    setDueDate(newDate) {
+        if(typeof newDate === "string" && newDate !== "") {
+            this.dueDate = newDate;
+        } else {
+            return false;
+        }
+    }
+
+    setPriority(newPriority) {
+        if(typeof newPriority === "string" && newPriority !== "") {
+            this.priority = newPriority;
+        } else {
+            return false;
+        }
+    }
+
+    setProject(newProject) {
+        if(typeof newProject === "string" && newProject !== "") {
+            this.project = newProject;
+        } else {
+            return false;
+        }
+    }
+
+    setComplete() {
+        this.complete = !this.complete;
     }
 }
