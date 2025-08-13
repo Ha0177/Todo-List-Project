4bd4f492b7c26e782c34b44780c1c5b48961450b
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-08-13 17:49:56 +0300

    Add Dom class for streamlined DOM manipulation methods

--- src/domManager.js
+++ src/domManager.js
@@ -1,3 +1,28 @@
 import { Task } from "./taskManager.js";
 import { Project, ProjectManager } from "./projectManager.js";
 
+class Dom {
+    static selectElement(selector) {
+        if (typeof selector === "string") {
+            return document.querySelector(selector);
+        }
+    }
+
+    static selectAllElements(selector) {
+        if (typeof selector === "string") {
+            return document.querySelectorAll(selector)
+        }
+    }
+
+    static addClass(element, className) {
+        if (typeof className === "string") {
+            element.classList.add(className);
+        }
+    }
+
+    static removeClass(element, className) {
+        if (typeof className === "string") {
+            element.classList.remove(className);
+        }
+    }
+}
