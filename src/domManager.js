fe171ac8dd9797f165a30a78e473eee43c0e0b59
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-08-14 20:06:33 +0300

    Implement initDOM function to initialize event listeners for adding projects and tasks and add project rendering

--- src/domManager.js
+++ src/domManager.js
@@ -1,5 +1,6 @@
 import { Task } from "./taskManager.js";
 import { Project, ProjectManager } from "./projectManager.js";
+import hashIcon from "./hash.svg";
 
 class Dom {
     static selectElement(selector) {
@@ -44,18 +45,60 @@ class Dom {
         if (options.id) {
             element.id = options.id;
         }
+        if (options.attributes) {
+            for (const [attrName, attrValue] of Object.entries(options.attributes)) {
+                element.setAttribute(attrName, attrValue);
+            }
+        }
 
         return element;
     }
 }
 
-function handleAddTaskBtnClick() {
-}
+function initDOM() {
 
-function handleAddProjectBtnClick() {
-    const addProjectDialog = Dom.selectElement("#add-project-dialog")
+    renderProjects();
+
+    const addTaskBtn = Dom.selectElement(".add-task-btn");
+    addTaskBtn.addEventListener("click", () => {
+        
+    });
+
+    //Project Handler
+
+    const addProjectBtn = Dom.selectElement(".add-project-btn");
+    const addProjectDialog = Dom.selectElement("#add-project-dialog")   
     const addProjectForm = Dom.selectElement("#add-project-form")
-    addProjectDialog.showModal();
+    const projectFormCancelBtn = Dom.selectElement("#project-form-cancel-btn");
+    const projectFormAddBtn = Dom.selectElement("#project-form-add-btn");
+
+    function renderProjects() {
+        const projectsContainer = Dom.selectElement("div.projects-container")
+        projectsContainer.textContent = "";
+        ProjectManager.projects.forEach((project) => {
+            const projectLink = Dom.createElement("a", {
+                class: "project-link"
+            })
+            const projectLinkIcon = Dom.createElement("img", {
+                class: "project-link-icon",
+                attributes: {
+                    src: hashIcon
+                }
+            })
+            const projectLinkName = Dom.createElement("span", {
+                class: "project-link-name",
+                textContent: project.name,
+            });
+            
+            projectsContainer.appendChild(projectLink);
+            projectLink.prepend(projectLinkName);
+            projectLink.prepend(projectLinkIcon);
+        });
+    };
+
+    addProjectBtn.addEventListener("click", () => {
+        addProjectDialog.showModal();
+    });
 
     addProjectDialog.addEventListener("click", (event) => {
         if (event.target === addProjectDialog) {
@@ -64,26 +107,24 @@ function handleAddProjectBtnClick() {
         }
     });
 
-    const projectFormCancelBtn = Dom.selectElement("#project-form-cancel-btn");
     projectFormCancelBtn.addEventListener("click", () => {
-       addProjectForm.reset(); 
-       addProjectDialog.close(); 
+        addProjectForm.reset(); 
+        addProjectDialog.close(); 
+     });
+
+     projectFormAddBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        const name = Dom.selectElement("input#project-name");
+        const color = Dom.selectElement("input#project-color")
+        if (name.value !== "") {
+            ProjectManager.addProject(name.value, color.value);
+        }
+        addProjectForm.reset();
+        addProjectDialog.close();
+        renderProjects();
     });
 
-    const projectFormAddBtn = Dom.selectElement("#project-form-add-btn");
-    projectFormAddBtn.addEventListener("click", () => {
-        const projectFormInput = Dom.selectElement("input#project-name");
-        ProjectManager.addProject(projectFormInput.value);
-    })
-}
-
-function renderDOM() {
-    const addTaskBtn = Dom.selectElement(".add-task-btn");
-    addTaskBtn.addEventListener("click", handleAddTaskBtnClick);
-
-    const addProjectBtn = Dom.selectElement(".add-project-btn");
-    addProjectBtn.addEventListener("click", handleAddProjectBtnClick);
 
 }
 
-export { Dom, renderDOM };
+export { Dom, initDOM };
